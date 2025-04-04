1ad5741cbebba41df090f63e9493b63a2d1441a5
Levi-Budd <21070@fraser.school.nz>
2025-04-04 14:18:14 +1300

    very rough and unfinished buttons, dont work properly yet.

--- 1_quiz_gui_v1.py
+++ 1_quiz_gui_v1.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from time import *
 import questions as q
+import tkinter as tk
 class Quiz():
     
     """
@@ -11,7 +12,9 @@ class Quiz():
         # this variable is the color grey used for the backround
         # makes it so you can change every widgets backround color
         # at the same time.
+        global background_color
         background_color = "#b3b3b3"
+        
         
         # this just sets the size of the window
         root.geometry("360x300")
@@ -25,41 +28,137 @@ class Quiz():
         # the heading is made here
         self.quiz_heading = Label(self.quiz_frame,
                                   text="Quiz Program",
-                                  font=("arial", "32"),
+                                  font=("arial", "25"),
                                   bg=background_color,
                                   )
         self.quiz_heading.grid(row=0)
         
         # the entrybox where you put in your answer is here
         self.quiz_entry = Entry(self.quiz_frame,
-                                  font=("Arial", "38"),
+                                  font=("Arial", "26"),
                                   width=(12),
                                   )
         self.quiz_entry.grid(row=2, padx=1, pady=1)
-        
-        self.quiz_entry_instructions_shadow = Label(self.quiz_frame,
-                                  text="Please enter your answer above",
-                                  font=("arial", "14"),
-                                  bg=background_color,
-                                  )
-        self.quiz_entry_instructions_shadow.place(x=30, y=120)
 
         self.quiz_entry_instructions_color = Label(self.quiz_frame,
                                   text="Please enter your answer above",
-                                  font=("arial", "14"),
+                                  font=("arial", "12"),
                                   fg="#FFFF00",
                                   bg=background_color,
                                   )
-        self.quiz_entry_instructions_color.place(x=5, y=0, in_=self.quiz_entry_instructions_shadow)
+        self.quiz_entry_instructions_color.grid(row=3, padx=1, pady=1,)
         # this is the end of the entry instructions frame
 
         #
         self.quiz_instructions = Label(self.quiz_frame,
-                                  text="blah",
-                                  font=("arial", "32"),
+                                  text="Press start quiz to start answering questions!",
+                                  font=("arial", "20"),
+                                  wraplength=250, width=20,
                                   bg=background_color,
                                   )
-        self.quiz_instructions.grid(row=4, pady=30)
+        self.quiz_instructions.grid(row=4)
+
+
+        self.button_frame = Frame(self.quiz_frame,
+                                bg=background_color,)
+        self.button_frame.grid()
+
+       
+        self.buttonswitch_left("default")
+        self.buttonswitch_right("default")
+        
+
+
+    def buttonswitch_right(self, case):
+        button_details_list = [
+            # text, color, command, row, column
+            # put your buttons features in this list.
+            ["Check results", "#aaaeff", lambda:self.buttonswitch_right("begin"), "1"],
+            ["finish", "#ffe600", lambda:self.buttonswitch_right("default"), "1"],
+        ]
+        self.button_ref_list = []
+
+        if case == "default":
+        #creates the default button that appears on program startup
+                self.make_button = Button(self.button_frame,
+                                        text=button_details_list[0][0],
+                                        bg=button_details_list[0][1],
+                                        fg="#000000",
+                                        font=("Arial", "15", "bold"),
+                                        width=9,
+                                        height=1,
+                                        command=button_details_list[0][2],
+                                        )
+                self.make_button.grid(column=1, padx=3, pady=5)
+                self.button_ref_list.append(self.make_button)
+
+        elif case == "begin":
+        #actually turns the variables into buttons
+                self.make_button = Button(self.button_frame,
+                                        text=button_details_list[1][0],
+                                        bg=button_details_list[1][1],
+                                        fg="#000000",
+                                        font=("Arial", "20", "bold"),
+                                        width=9,
+                                        height=1,
+                                        command=button_details_list[1][2]
+                                        )
+                self.make_button.grid(column=1, padx=3, pady=5)
+                self.button_ref_list.append(self.make_button)
+        
+    def buttonswitch_left(self, case):
+        button_details_list = [
+            # text, color, command, row, column
+            # put your buttons features in this list.
+            ["Start Quiz", "#00ff08", lambda:self.buttonswitch_left("begin"), "0"],
+            ["Submit", "#00ff08", lambda:self.buttonswitch_left("end"), "0"],
+            ["Submit", "#00ff08", lambda:print("sample text"), "0"],
+        ]
+        self.button_ref_list = []
+
+        if case == "default":
+        #creates the default button that appears on program startup
+                self.make_button = Button(self.button_frame,
+                                        text=button_details_list[0][0],
+                                        bg=button_details_list[0][1],
+                                        fg="#000000",
+                                        font=("Arial", "20", "bold"),
+                                        width=9,
+                                        height=1,
+                                        command=button_details_list[0][2]
+                                        )
+                self.make_button.grid(column=0, padx=3, pady=5)
+                self.button_ref_list.append(self.make_button)
+
+        elif case == "begin":
+        #actually turns the variables into buttons
+                self.make_button = Button(self.button_frame,
+                                        text=button_details_list[1][0],
+                                        bg=button_details_list[1][1],
+                                        fg="#000000",
+                                        font=("Arial", "20", "bold"),
+                                        width=9,
+                                        height=1,
+                                        command=button_details_list[1][2]
+                                        )
+                self.make_button.grid(column=0, padx=3, pady=5)
+                self.button_ref_list.append(self.make_button)
+
+        elif case == "end":
+    #actually turns the variables into buttons
+            self.make_button = Button(self.button_frame,
+                                    text=button_details_list[2][0],
+                                    bg=button_details_list[2][1],
+                                    fg="#000000",
+                                    font=("Arial", "20", "bold"),
+                                    width=9,
+                                    height=1,
+                                    state="disabled",
+                                    command=button_details_list[2][2]
+                                    )
+            self.make_button.grid(column=0, padx=3, pady=5)
+            self.button_ref_list.append(self.make_button)
+                
 
         
 
---
+++ test.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+MyWindow = tk.Tk()
+MyWindow.geometry("500x550")
+
+#create LabelFrame (200x200)
+label = tk.LabelFrame(MyWindow, width=200, height=200)
+
+#grid manager to set label localization
+label.grid(row=0, column=0)
+
+#label row and column configure: first argument is col or row id
+label.grid_rowconfigure(0, weight=1)
+label.grid_columnconfigure(0, weight=1)
+
+#cancel propagation
+label.grid_propagate(False)
+
+#Create button and set it localization. You can change it font without changing size of button, but if You set too big not whole will be visible
+button = tk.Button(label, text="Hello!", font=('Helvetica', '20'))
+
+#Use sticky to button took up the whole label area
+button.grid(row=0, column=0, sticky='nesw')
+
+MyWindow.mainloop()
