0322a783066e778e33a10472a9e49891392bdda6
Levi-Budd <21070@fraser.school.nz>
2025-05-15 11:41:52 +1200

    Trim leading/trailing spaces from answers, add character limit to answer input, finish button now opens results window, fixed bug where entry box and enter key stayed enabled after quiz end, added question result summary to results.txt (correct/incorrect count), and added comments.

--- 1_quiz_gui_v1.py
+++ 1_quiz_gui_v1.py
@@ -26,6 +26,8 @@ class Quiz():
     correct = 0
     incorrect = 0
     early_finish = False
+    # channge answer_length to change the max length of the answer.
+    answer_length = 30
    
 
     def __init__(self, root):
@@ -101,7 +103,7 @@ class Quiz():
             # text, color, command, row, column
             # put your buttons features in list.
             ["results", "#aaaeff", lambda: self.open_results(), "normal"],
-            ["finish", "#ffe600", lambda: [setattr(self, "early_finish", True), self.buttonswitch_left("end")], "normal"],
+            ["finish", "#ffe600", lambda: [setattr(self, "early_finish", True), self.buttonswitch_left("end"), self.open_results()], "normal"],
         ]
         self.right_button_ref_list = []
 
@@ -151,6 +153,10 @@ class Quiz():
                 # grab the current time and date for the end of the quiz.
                 self.start_date = strftime("%Y-%m-%d %H:%M:%S", localtime())
                 self.write_to_file(self. results)
+                # i unbind the enter key so you cant keep calling the function after the quiz is finished.
+                root.unbind('<Return>')
+                # disable the entry box so you cant enter anything after the quiz is finished.
+                self.quiz_entry.config(state=DISABLED)
         
         # this statement creates the button depending on the state variable.
         self.make_button = Button(self.button_frame,
@@ -201,16 +207,24 @@ class Quiz():
                 if self.counter > 0:
                     # counter_less_1 is used because lists start at 0, not 1.
                     counter_less_1 = self.counter - 1
-
-                    answer = self.quiz_entry.get().lower()
+                    # block below sets answer to lowercase, remove leading and trailing spaces, and limit to first 30 characters.
+                    answer = self.quiz_entry.get().strip().lower()
+                    answer = answer[0:self.answer_length]
                     self.quiz_entry_instructions_color.config(text="Please enter your answer above", fg="#FFFF00")
-                    data_list.append([self.counter, q.Questions[counter_less_1], self.quiz_entry.get()])
+
+                    # appends the question number, question text, and answer to the data_list.
+                    data_list.append([self.counter, q.Questions[counter_less_1], answer])
+
+                    # this clears the entry box so you can enter the next answer, gets the correct answer.
                     self.quiz_entry.delete(0, END)
                     correct_answer = q.Answers[counter_less_1]
+                    
+                    # this block gets the time taken for the question, and appends it to the data_list.
                     end_taken_time = time.time()
                     
 
                     # this just changes the text depending on if the answer is correct or not.
+                    # it also appends the correctness to the data_list.
                     if answer in correct_answer:
                         self.quiz_entry_instructions_color.config(text="Correct!", fg="#00ff08")
                         data_list.append(["correct"])
@@ -219,10 +233,13 @@ class Quiz():
                         self.quiz_entry_instructions_color.config(text="Incorrect!", fg="#ff0000")
                         data_list.append(["incorrect"])
                         self.incorrect += 1
+
+                    # math getting the time taken for the question.
                     elapsed_time = end_taken_time - self.start_taken_time
                     minutes = int(elapsed_time // 60)
                     seconds = round(elapsed_time % 60, 2)
 
+                    # this appends the time taken to the data_list.
                     data_list.append([minutes, seconds])
                     data_list = sum(data_list, [])
                     self.results.append(data_list)
@@ -242,8 +259,7 @@ class Quiz():
                 self.open_results()
                 self.quiz_entry.config(state=DISABLED)
                 self.quiz_entry_instructions_color.config(text="Quiz Finished! go to results page for results!", fg="#00FFFF")
-                #i unbind the enter key so you cant keep calling the function after the quiz is finished.
-                root.unbind('<Return>')
+
 
     def write_to_file(self, data):
         with open("results.txt", "a") as r:
@@ -260,13 +276,13 @@ class Quiz():
 -------------------------------------------TEST RESULTS----------------------------------------------------------
 Name: placeholder
 Test started: {date}
+Questions correct: {self.correct}/{self.length}
 The formatting is as follows:
 Question Number : Question Text : Given Answer : Correct or Wrong : minutes : seconds
 """)        
-            
+            # this loops through the data to write each question's details
             tracker = self.counter + 1  # Adjust for 0-based index
             for tracker in range(len(data)):
-                # tracker = tracker - 1  # Adjust for 0-based index
                 question_number = data[tracker][0]  # Question number (start from 0)
                 question_text = data[tracker][1]  # Question text
                 given_answer = data[tracker][2]  # Given answer
@@ -278,6 +294,8 @@ Question Number : Question Text : Given Answer : Correct or Wrong : minutes : se
                 r.write(f"""
 {question_number} : {question_text} : {given_answer} : {correctness} : {minutes} : {seconds}
 """)        
+                
+            # check if the quiz was finished early, and sets early_finish_text to the string if it was.
             early_finish_text = ""
             if self.early_finish == True:
                  early_finish_text = "Quiz was finished early."
@@ -287,6 +305,7 @@ Test Finished in: {elapsed_minutes} minutes and {elapsed_seconds} seconds.
 {early_finish_text}
 
 """)
+            # this block writes the summary results to summart_results.txt.
             r.close
             with open("summary_results.txt", "a") as r:
  
@@ -296,85 +315,89 @@ Test Finished in: {elapsed_minutes} minutes and {elapsed_seconds} seconds.
             
 
 class ResultsWindow():
-      def __init__(self, parent):
-            self.results_window = Toplevel(parent)
-            self.results_window.title("Results")
-            self.results_window.geometry("360x425")
-            self.results_window.configure(bg=background_color)
+    """
+    This class creates the results window, which shows the results of the quiz.
+    """
 
-            self.results_frame = Frame(bg=background_color, master=self.results_window)
-            self.results_frame.grid(sticky="n")
-            self.results_window.grid_rowconfigure(0, weight=1)
-            self.results_window.grid_columnconfigure(0, weight=1)
-            
-            self.results_heading = Label(self.results_frame,
-                                  text="Results",
-                                  font=("arial", "25"),
-                                  
-                                  bg=background_color,
-                                  )            
-            self.results_heading.grid(row=0, column=0, sticky="n")
+    def __init__(self, parent):
+        self.results_window = Toplevel(parent)
+        self.results_window.title("Results")
+        self.results_window.geometry("360x425")
+        self.results_window.configure(bg=background_color)
 
-            self.results_label = Label(self.results_frame,
-                                  text="These are the results of the 5 most \n"
-                                       " recent quizzes out of X total\n"
-                                       " quizzes, the lower the older.",
-                                  font=("arial", "14"),
-                                  justify="left",
-                                  wraplength=350,
-                                  bg=background_color,
-                                  )
-            self.results_label.grid(row=1, column=0)
-            self.results_list = Listbox(self.results_frame,
-                                  font=("Arial", "14"),
-                                  width=27,
-                                  height=5,
-                                  bg='lime',
-                                  )
-            self.results_list.grid(row=2, column=0, padx=1, pady=1)
-            
-            # Define a method to show results
-            def show_results():
-                with open("summary_results.txt", "r") as r:
-                    lines = r.readlines()  # Read all lines from the file
-                    for i in range(min(5, len(lines))):  # Check the last 5 lines
-                        line = lines[-(i + 1)].strip()  # Access lines from the bottom up
-                        self.results_list.insert(END, line)  # Insert the line into the results_list
+        self.results_frame = Frame(bg=background_color, master=self.results_window)
+        self.results_frame.grid(sticky="n")
+        self.results_window.grid_rowconfigure(0, weight=1)
+        self.results_window.grid_columnconfigure(0, weight=1)
+        
+        self.results_heading = Label(self.results_frame,
+                                text="Results",
+                                font=("arial", "25"),
+                                
+                                bg=background_color,
+                                )            
+        self.results_heading.grid(row=0, column=0, sticky="n")
 
-            
-            # Call the method to insert the result
-            show_results()
+        self.results_label = Label(self.results_frame,
+                                text="These are the results of the 5 most \n"
+                                    " recent quizzes out of X total\n"
+                                    " quizzes, the lower the older.",
+                                font=("arial", "14"),
+                                justify="left",
+                                wraplength=350,
+                                bg=background_color,
+                                )
+        self.results_label.grid(row=1, column=0)
+        self.results_list = Listbox(self.results_frame,
+                                font=("Arial", "14"),
+                                width=27,
+                                height=5,
+                                bg='lime',
+                                )
+        self.results_list.grid(row=2, column=0, padx=1, pady=1)
+        
+        # Define a method to show results
+        def show_results():
+            with open("summary_results.txt", "r") as r:
+                lines = r.readlines()  # Read all lines from the file
+                for i in range(min(5, len(lines))):  # Check the last 5 lines
+                    line = lines[-(i + 1)].strip()  # Access lines from the bottom up
+                    self.results_list.insert(END, line)  # Insert the line into the results_list
 
-            self.results_info = Label(self.results_frame,
-                                  text="You can see more in-depth results (what questions were wrong specifically) by going to the file. If the box is yellow, then there are more results in the file than shown here.",                                       
-                                  font=("arial", "14"),
-                                  justify="left",
-                                  wraplength=350,
-                                  bg=background_color,
-                                  )
-            self.results_info.grid(row=3, column=0)
+        
+        # Call the method to insert the result
+        show_results()
 
-            self.results_button_frame = Frame(self.results_frame,
-                                     background=background_color)
-            self.results_button_frame.grid(sticky="ew", row=4)
-            results_button_details_list = [
-            # text, color, command, row, column
-            # put your buttons features in this list.
-            ["Back", "#f44336", lambda:self.results_window.destroy(), "0", "0"],
-            ["To File", "#aaaeff", lambda: os.startfile("results.txt"), "0", "1"],
-            ]
-            self.button_ref_list = []
-            #actually turns the variables into buttons
-            for item in results_button_details_list:
-                self.make_button = Button(self.results_button_frame,
-                                    text=item[0],
-                                    bg=item[1],
-                                    fg="#000000",
-                                    font=("Arial", "20", "bold"),
-                                    width=9,
-                                    command=item[2]
-                                    )
-                self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
+        self.results_info = Label(self.results_frame,
+                                text="You can see more in-depth results (what questions were wrong specifically) by going to the file. If the box is yellow, then there are more results in the file than shown here.",                                       
+                                font=("arial", "14"),
+                                justify="left",
+                                wraplength=350,
+                                bg=background_color,
+                                )
+        self.results_info.grid(row=3, column=0)
+
+        self.results_button_frame = Frame(self.results_frame,
+                                    background=background_color)
+        self.results_button_frame.grid(sticky="ew", row=4)
+        results_button_details_list = [
+        # text, color, command, row, column
+        # put your buttons features in this list.
+        ["Back", "#f44336", lambda:self.results_window.destroy(), "0", "0"],
+        ["To File", "#aaaeff", lambda: os.startfile("results.txt"), "0", "1"],
+        ]
+        self.button_ref_list = []
+        #actually turns the variables into buttons
+        for item in results_button_details_list:
+            self.make_button = Button(self.results_button_frame,
+                                text=item[0],
+                                bg=item[1],
+                                fg="#000000",
+                                font=("Arial", "20", "bold"),
+                                width=9,
+                                command=item[2]
+                                )
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
 
 
 
--- questions.py
+++ questions.py
@@ -1,27 +1,55 @@
 """
 This is where the questions and answers are, put them in the quotes.
 """
+
 Questions = [
-    'how many apples in a dozen apples',
-    'What is the color of the sky on a clear day?',
-    'How many legs does a dog have?',
-    'What is 2 + 2?',
-    'What do you call the frozen form of water?',
-    'What is the opposite of "hot"?',
-    'What animal goes "meow"?',
-    'What fruit is typically red and often associated with doctors?',
-    'How many days are in a week?',
-    'What is the first letter of the alphabet?',
+    "How many apples are in a dozen apples?",
+    "What's the color of the sky on a clear day?",
+    "How many legs does a dog have?",
+    "What's 2 + 2?",
+    "What do you call the frozen form of water?",
+    "What's the opposite of 'hot'?",
+    "What animal goes 'meow'?",
+    "What fruit's typically red and often associated with doctors?",
+    "How many days are in a week?",
+    "What's the first letter of the alphabet?",
 ]
 Answers = [
-    ['12', 'twelve', '1', 'one', 'dozen', 'a dozen'],
-    ['blue'],
-    ['4', 'four'],
-    ['4', 'four'],
-    ['ice', 'the frozen form of water'],
-    ['cold', "toh"],
-    ['cat', 'cats', 'human', 'humans'],
-    ['apple', 'apples'],
-    ['7', 'seven'],
-    ['a', 't']
+    ["12", "twelve", "1", "one", "dozen", "a dozen"],
+    ["blue"],
+    ["4", "four"],
+    ["4", "four"],
+    ["ice", "the frozen form of water"],
+    ["cold", "toh"],
+    ["cat", "cats", "human", "humans"],
+    ["apple", "apples"],
+    ["7", "seven"],
+    ["a", "t"],
 ]
+
+"""
+Questions = [
+    "What's the main ingredient in guacamole?",
+    "What type of pasta's shaped like little rice grains?",
+    "Which fruit's known as the 'king of fruits'?",
+    "What's the primary ingredient in hummus?",
+    "What type of cheese's traditionally used on a Margherita pizza?",
+    "Which spice's known as the most expensive in the world?",
+    "What's the name of the French dish made of snails?",
+    "What's the main ingredient in tofu?",
+    "Which fruit's dried to make raisins?",
+    "What's the name of the Japanese dish made of raw fish and rice?",
+]
+Answers = [
+    ["avocado", "avocados"],
+    ["orzo"],
+    ["mango", "durian"],
+    ["chickpeas", "garbanzo beans"],
+    ["mozzarella"],
+    ["saffron"],
+    ["escargot"],
+    ["soybeans", "soy"],
+    ["grapes"],
+    ["sushi"],
+]
+"""
